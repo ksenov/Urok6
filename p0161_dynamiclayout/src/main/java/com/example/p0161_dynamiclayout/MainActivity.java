50f45b24863da349a33a3218271e17fb573610a6
Ksenofontov Boris <xenofontb@gmail.com>
2021-02-13 12:28:06 +0900

    Initial commit

---
+++ p0161_dynamiclayout/src/main/java/com/example/p0161_dynamiclayout/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.p0161_dynamiclayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // создание LinearLayout
+        LinearLayout linLayout = new LinearLayout(this);
+        // установим вертикальную ориентацию
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        // создаем LayoutParams
+        ViewGroup.LayoutParams linLayoutParam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        // устанавливаем linLayout как корневой элемент экрана
+        setContentView(linLayout, linLayoutParam);
+
+        // 1
+        ViewGroup.LayoutParams lpView = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        TextView tv = new TextView(this);
+        tv.setText("TextView");
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv);
+
+        Button btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
+
+        // 2
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
+
+        Button btn1 = new Button(this);
+        btn1.setText("Button1");
+        linLayout.addView(btn1, leftMarginParams);
+
+        // 3
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button btn2 = new Button(this);
+        btn2.setText("Button2");
+        linLayout.addView(btn2, rightGravityParams);
+    }
+}
